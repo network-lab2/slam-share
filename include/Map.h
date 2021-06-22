20162133bbadcb7359d2319cbdf72e4f96817d6d
Aditya <adityadhakal@github.com>
2021-06-22 13:48:45 +0200

    see which classes derive from wheere

--- include/Map.h
+++ include/Map.h
@@ -40,7 +40,7 @@ namespace ORB_SLAM3
 
 //Define an STL compatible allocator of ints that allocates from the managed_shared_memory.
 //This allocator will allow placing containers in the segment
-typedef boost::interprocess::allocator<int, managed_shared_memory::segment_manager>  ShmemAllocator;
+typedef boost::interprocess::allocator<int, boost::interprocess::managed_shared_memory::segment_manager>  ShmemAllocator;
 
 
 class MapPoint;
