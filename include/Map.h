fa866e69db36d46c3a631dc01d763e49ded2d444
Aditya <adityadhakal@github.com>
2021-06-22 13:56:59 +0200

    changed the namespace to remove boost interprocess.

--- include/Map.h
+++ include/Map.h
@@ -33,7 +33,7 @@
 #include <boost/interprocess/managed_shared_memory.hpp>
 #include <boost/serialization/base_object.hpp>
 
-using namespace boost::interprocess;
+//using namespace boost::interprocess;
 
 
 namespace ORB_SLAM3
@@ -41,7 +41,7 @@ namespace ORB_SLAM3
 
 //Define an STL compatible allocator of ints that allocates from the managed_shared_memory.
 //This allocator will allow placing containers in the segment
-typedef boost::interprocess::allocator<int, managed_shared_memory::segment_manager>  ShmemAllocator;
+typedef boost::interprocess::allocator<int, boost::interprocess::managed_shared_memory::segment_manager>  ShmemAllocator;
 
 
 class MapPoint;
