bffdb0720b0463136cb347587d01190a1254814e
Aditya <adityadhakal@github.com>
2021-07-06 13:53:00 +0200

    voided

--- src/System.cc
+++ src/System.cc
@@ -163,7 +163,8 @@ cv::Mat System::TrackStereo(const cv::Mat &imLeft, const cv::Mat &imRight, const
 
     //mpTracker = new Tracking(this, mpVocabulary, mpFrameDrawer, mpMapDrawer,
     //                         mpAtlas, mpKeyFrameDatabase, strSettingsFile, mSensor, strSequence);
-    mpTracker = segment.find_or_construct<Tracking>("TrackingThread")();
+    mpTracker = segment.find_or_construct<Tracking>("TrackingThread")(void, void, void, void,
+                             void, void, void, void, void);
 
 
     if(mSensor!=STEREO && mSensor!=IMU_STEREO)
