89faac2ac65b48245f648bcaea789ad67c231e26
Aditya <adityadhakal@github.com>
2021-07-06 15:12:33 +0200

    seg

--- src/System.cc
+++ src/System.cc
@@ -172,7 +172,7 @@ cv::Mat System::TrackStereo(const cv::Mat &imLeft, const cv::Mat &imRight, const
 
     //mpTracker = new Tracking(this, mpVocabulary, mpFrameDrawer, mpMapDrawer,
     //                         mpAtlas, mpKeyFrameDatabase, strSettingsFile, mSensor, strSequence);
-    mpTracker = segment.find<Tracking>("TrackingThread")(nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,"",0,"");
+    mpTracker = (*seg).find<Tracking>("TrackingThread")(nullptr,nullptr,nullptr,nullptr,nullptr,nullptr,"",0,"");
     cout<<"Here.."<<endl;
     cout<<"name of managed shared memory: "<<boost::interprocess::managed_shared_memory::get_instance_name(mpTracker)<<endl;
     cout<<"offset_tracker "<<offset_tracker->mSensor<<endl;
