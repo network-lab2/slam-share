95223b72eacbd16d9e01e316e75d9a54cf2e932e
Aditya <adityadhakal@github.com>
2021-07-06 13:54:44 +0200

    just find

--- src/System.cc
+++ src/System.cc
@@ -163,8 +163,7 @@ cv::Mat System::TrackStereo(const cv::Mat &imLeft, const cv::Mat &imRight, const
 
     //mpTracker = new Tracking(this, mpVocabulary, mpFrameDrawer, mpMapDrawer,
     //                         mpAtlas, mpKeyFrameDatabase, strSettingsFile, mSensor, strSequence);
-    mpTracker = segment.find_or_construct<Tracking>("TrackingThread")(nullptr, nullptr, nullptr, nullptr,
-                             nullptr, nullptr, nullptr, nullptr, nullptr);
+    mpTracker = segment.find<Tracking>("TrackingThread")();
 
 
     if(mSensor!=STEREO && mSensor!=IMU_STEREO)
