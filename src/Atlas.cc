226ed10781332d6d2a2308acb5c9c70cda92167f
Aditya <adityadhakal@github.com>
2021-07-08 00:14:49 +0200

    what now

--- src/Atlas.cc
+++ src/Atlas.cc
@@ -100,7 +100,7 @@ void Atlas::CreateNewMap()
 
     //initialize the map now.
 
-    mpCurrentMap = segment->find_or_construct<Map>("Map1") (mnLastInitKFidMap);
+    mpCurrentMap = ORB_SLAM3::segment.find_or_construct<Map>("Map1") (mnLastInitKFidMap);
     cout<<"Created Map object in shared memory! Address is: "<<mpCurrentMap<<endl;
     cout<<"Reading a variable there "<<mpCurrentMap->GetMaxKFid()<<endl;
 
