da38f02e201be6457c7a3234fc12d59b388c0d1c
Aditya <adityadhakal@github.com>
2021-07-09 15:06:35 +0200

    ...

--- src/Atlas.cc
+++ src/Atlas.cc
@@ -245,8 +245,8 @@ void Atlas::clearAtlas()
     mnLastInitKFidMap = 0;
 }
 
-//boost::interprocess::offset_ptr<Map>  Atlas::GetCurrentMap()
-Map* Atlas::GetCurrentMap()
+boost::interprocess::offset_ptr<Map>  Atlas::GetCurrentMap()
+//Map* Atlas::GetCurrentMap()
 {
     //boost::interprocess::managed_shared_memory segment(boost::interprocess::open_or_create, "MySharedMemory",10737418240);
     Atlas *atl = segment->find_or_construct<Atlas>("Atlas")();
