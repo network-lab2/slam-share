0b1339dc3935d322e77e1e66e80ff6e1d9529da2
Aditya <adityadhakal@github.com>
2021-07-27 21:48:36 +0200

    changed some extern variables

--- include/System.h
+++ include/System.h
@@ -46,8 +46,10 @@
 namespace ORB_SLAM3
 {
    //GLOBAL Variable
-extern boost::interprocess::managed_shared_memory segment;//(boost::interprocess::open_or_create, "MySharedMemory",10737418240);
+extern boost::interprocess::fixed_managed_shared_memory segment;//(boost::interprocess::open_or_create, "MySharedMemory",10737418240);
 extern boost::interprocess::mapped_region region;
+ //Create an allocator that allocates ints from the managed segment
+extern boost::interprocess::allocator<char, boost::interprocess::managed_shared_memory::segment_manager> allocator_instance;
 
 class Verbose
 {
