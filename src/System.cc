c1a0625c2c15efd6411a6d1e02a3dc71124f2aa4
Aditya <adityadhakal@github.com>
2021-08-24 13:53:18 +0200

    probably pointer mistake?

--- src/System.cc
+++ src/System.cc
@@ -142,7 +142,7 @@ System::System(const string &strVocFile, const string &strSettingsFile, const eS
     mpAtlas = (segment.find<Atlas>("Atlas")).first;
    
     std::cout<<"Incrementing the magic number\n";
-    *magic_num++;
+    *magic_num = *magic_num+1;
     std::cout<<"New magic number"<<*magic_num<<std::endl;
 
     if(0 == mpAtlas){
