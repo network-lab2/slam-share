3b531fd8202b4dcd9d7f8beba64045391596fbe2
Aditya <adityadhakal@github.com>
2021-09-07 23:50:05 +0200

    get number MPS

--- src/System.cc
+++ src/System.cc
@@ -910,7 +910,7 @@ void System::PostLoad(){
         std::cout<<"Point distribution of Other Atlas: (mnFrameID) "<<std::endl;
         otherAtlas->currentMapPtr->GetOriginKF()->PrintPointDistribution();
         std::cout<<std::endl;
-        std::cout<<"Other atlas timestamp: "<<otherAtlas->currentMapPtr->GetOriginKF()->NLeft<<"\n";
+        std::cout<<"Other atlas timestamp: "<<otherAtlas->currentMapPtr->GetOriginKF()->GetNumberMPs()<<"\n";
         std::cout<<"Num of mappoints to currentMapPtr in OTHER atlas: "<<otherAtlas->currentMapPtr->MapPointsInMap()<<std::endl;
         std::cout<<"Num of mappoints to currentMapPtr in CURRENT atlas: "<<mpAtlas->currentMapPtr->MapPointsInMap()<<std::endl;
 
@@ -936,7 +936,7 @@ void System::PostLoad(){
         otherAtlas->currentMapPtr->GetOriginKF()->PrintPointDistribution();
         std::cout<<std::endl;
         std::cout<<"Num of mappoints to currentMapPtr in THIS atlas: "<<otherAtlas->currentMapPtr->MapPointsInMap()<<std::endl;
-    }   std::cout<<"Other atlas timestamp: "<<otherAtlas->currentMapPtr->GetOriginKF()->NLeft<<"\n";
+    }   std::cout<<"Other atlas timestamp: "<<otherAtlas->currentMapPtr->GetOriginKF()->GetNumberMPs()<<"\n";
 
 }
 
