6e771a9083cec1186a770c9bf7e8a71dc99abdbe
Aditya <adityadhakal@github.com>
2021-10-11 17:00:35 +0200

    ..

--- include/KeyFrame.h
+++ include/KeyFrame.h
@@ -528,8 +528,8 @@ public:
     //std::vector< std::vector <std::vector<size_t> > > mGridRight;
     //new-code
     //boost::interprocess::offset_ptr<size_t_vector_vector_vector> mGridRight;
-    boost::interprocess::offset_ptr<Matrix<size_t> > matrix_mgridright;
-    boost::interprocess::offset_ptr<Matrix<size_t> > mGridRight;
+    boost::interprocess::offset_ptr<Matrix_1<size_t> > matrix_mgridright;
+    boost::interprocess::offset_ptr<Matrix_1<size_t> > mGridRight;
     //std::size_t mGridRight[FRAME_GRID_COLS][FRAME_GRID_ROWS];
     //const std::vector<size_t> buffer_mGridRight;
 
