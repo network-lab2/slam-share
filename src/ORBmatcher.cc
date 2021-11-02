a7c89b6bc83a606cba3a25b6da6afcd14b1331c7
Aditya <adityadhakal@github.com>
2021-11-02 15:53:38 +0100

    *** search by bow.

--- src/ORBmatcher.cc
+++ src/ORBmatcher.cc
@@ -273,6 +273,7 @@ bool ORBmatcher::CheckDistEpipolarLine2(const cv::KeyPoint &kp1, const cv::KeyPo
 int ORBmatcher::SearchByBoW(boost::interprocess::offset_ptr<KeyFrame>  pKF,Frame &F, vector<boost::interprocess::offset_ptr<MapPoint> > &vpMapPointMatches)
 {
     const vector<boost::interprocess::offset_ptr<MapPoint> > vpMapPointsKF = pKF->GetMapPointMatches();
+    std::cout<<"SearchByBow: after GetMapPointMatches"<<std::endl;
 
     vpMapPointMatches = vector<boost::interprocess::offset_ptr<MapPoint> >(F.N,static_cast<boost::interprocess::offset_ptr<MapPoint> >(NULL));
 
