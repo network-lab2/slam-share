03a565777862c41c159ba5d774ab3f2840659d39
Aditya <adityadhakal@github.com>
2021-11-02 04:31:09 +0100

    just checking the essential match.

--- src/System.cc
+++ src/System.cc
@@ -945,7 +945,7 @@ void System::PostLoad(){
         std::cout<<"Number of Keyframes after adding the map: "<<mpAtlas->KeyFramesInMap()<<std::endl;
 
 
-        otherAtlas->currentMapPtr->PrintEssentialGraph();
+        otherAtlas->currentMapPtr->CheckEssentialGraph();
 
         //now update the mpAtlas to newly read map
         //std::cout<<"Changing the map to other atlas.\n";
