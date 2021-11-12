37e52cefbb06b3af425b4b76cfbb9771c23963ea
Aditya <adityadhakal@github.com>
2021-11-12 21:46:16 +0100

    newdetect common regions.

--- src/System.cc
+++ src/System.cc
@@ -977,6 +977,7 @@ void System::PostLoad(){
 
         std::cout<<"Add the existing keyframes from map to loopcloser to just seer if it works\n";
         std::vector<boost::interprocess::offset_ptr<KeyFrame> > vkf = mpAtlas->currentMapPtr->GetAllKeyFrames();
+        mpLoopCloser->NewDetectCommonRegions();
 
         for(auto k : vkf){
                     //cout << "------press enter to continue------vkf size: " << vkf.size() << endl;
