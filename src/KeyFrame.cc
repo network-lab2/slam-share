a674fe92c1417adaa1c42be13c79591af0a79aab
Aditya <adityadhakal@github.com>
2021-11-15 19:48:45 +0100

    checkout new static function.

--- src/KeyFrame.cc
+++ src/KeyFrame.cc
@@ -452,7 +452,7 @@ void KeyFrame::FixMatrices(boost::interprocess::offset_ptr<KeyFrame> pKF)
    
 }
 
-void KeyFrame::FixBow(boost::interprocess::offset_ptr<KeyFrame> pKF,ORBVocabulary* mpORBVocabulary_2){
+static void KeyFrame::FixBow(boost::interprocess::offset_ptr<KeyFrame> pKF,ORBVocabulary* mpORBVocabulary_2){
     //if(pKF->mBowVec.empty() || pKF->mFeatVec.empty())
     //{
         //memcpy(&(pKF->mBowVec), &mBowVec, sizeof(DBoW2::BowVector));
