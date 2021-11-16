86bd715c81be18fbb7d2f5069d27262c3620ae29
Aditya <adityadhakal@github.com>
2021-11-16 11:04:13 +0100

    check more prompts.

--- src/KeyFrameDatabase.cc
+++ src/KeyFrameDatabase.cc
@@ -43,7 +43,7 @@ void KeyFrameDatabase::add(boost::interprocess::offset_ptr<KeyFrame> pKF)
     //std::cout<<"KeyFrameDatabase::after lock. mBowVec Size: "<<pKF->mBowVec.size()<<" mvInvertedFile size: "<<mvInvertedFile.size()<<std::endl;
     int counter = 0;
     for(DBoW2::BowVector::const_iterator vit= pKF->mBowVec.begin(), vend=pKF->mBowVec.end(); vit!=vend; vit++){
-        //std::cout<<"BowVec: "<<vit->first<<" Counter: "<<counter<<std::endl;
+        std::cout<<"BowVec Size: "<<pKF->mBowVec.size()<<" Counter: "<<counter<<std::endl;
         counter++;
         mvInvertedFile[vit->first].push_back(pKF);
         if(counter>=pKF->mBowVec.size())
