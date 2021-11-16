4217239fa61711afc76330b88be36ded31672e47
Aditya <adityadhakal@github.com>
2021-11-16 05:42:42 +0100

    check the mbowvec.

--- src/KeyFrame.cc
+++ src/KeyFrame.cc
@@ -472,9 +472,9 @@ void KeyFrame::FixMatrices(boost::interprocess::offset_ptr<KeyFrame> pKF)
         std::cout<<"Or fails here. vCurrentDesc size: "<<vCurrentDesc.size()<<std::endl;
         mpORBvocabulary->transform(vCurrentDesc,*newBow,*newFeat,4);
         std::cout<<"Transform passed\n";
-        pKF->mBowVec = *newBow;
+        //pKF->mBowVec = *newBow;
         memcpy(&(pKF->mBowVec), newBow, sizeof(DBoW2::BowVector));
-        std::cout<<"first memcpy passed\n";
+        //std::cout<<"first memcpy passed\n";
         memcpy(&(pKF->mFeatVec), newFeat, sizeof(DBoW2::FeatureVector));
         
         //pKF->mBowVec = newBow;
