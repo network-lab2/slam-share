6926fdddc7eac96aa9540b4b2fa523ebff4eaa5c
Aditya <adityadhakal@github.com>
2021-11-18 20:31:43 +0100

    works?

--- src/KeyFrame.cc
+++ src/KeyFrame.cc
@@ -459,7 +459,7 @@ void KeyFrame::FixMatrices(boost::interprocess::offset_ptr<KeyFrame> pKF)
     //update the PID
     ownerProcess = getpid();
 
-    UpdateBestCovisibles();
+    //UpdateBestCovisibles();
     //std::cout<<"Updated the Best Covisibles--"<<std::endl;
 
    
@@ -490,7 +490,7 @@ void KeyFrame::FixMatrices(boost::interprocess::offset_ptr<KeyFrame> pKF)
         // Feature vector associate features with nodes in the 4th level (from leaves up)
         // We assume the vocabulary tree has 6 levels, change the 4 otherwise
         //std::cout<<"Or fails here. vCurrentDesc size: "<<vCurrentDesc.size()<<std::endl;
-        mpORBvocabulary->transform(vCurrentDesc,*newBow,*newFeat,4);
+        mpORBVocabulary_2->transform(vCurrentDesc,*newBow,*newFeat,4);
         //std::cout<<"Transform passed\n";
         //pKF->mBowVec = *newBow;
         memcpy(&(pKF->mBowVec), newBow, sizeof(DBoW2::BowVector));
