2a9217c5caf345c2f264633621e2848639895edd
Aditya <adityadhakal@github.com>
2022-01-10 15:10:19 +0100

    score new?

--- Thirdparty/DBoW2/DBoW2/TemplatedVocabulary.h
+++ Thirdparty/DBoW2/DBoW2/TemplatedVocabulary.h
@@ -1217,7 +1217,7 @@ inline double TemplatedVocabulary<TDescriptor,F>::score
 
 
 template<class TDescriptor, class F> 
-inline double TemplatedVocabulary<TDescriptor,F>::score
+inline double TemplatedVocabulary<TDescriptor,F>::score_new
   (const boost::interprocess::offset_ptr<bowMap> v1, const boost::interprocess::offset_ptr<bowMap> v2) const
 {
   return m_scoring_object->score_new(v1, v2);
