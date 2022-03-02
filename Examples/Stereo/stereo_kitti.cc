6561820eab0761d3021cce181aefe81dab58860b
Aditya <adityadhakal@github.com>
2022-03-02 09:50:52 +0100

    don't drop.

--- Examples/Stereo/stereo_kitti.cc
+++ Examples/Stereo/stereo_kitti.cc
@@ -284,7 +284,7 @@ void LoadImages(const string &strPathToSequence, vector<string> &vstrImageLeft,
     string strPathTimeFile = strPathToSequence + "/times.txt";
     fTimes.open(strPathTimeFile.c_str());
     int counter = 0;
-    int skip = 2; //skip one in 300
+    int skip = 10000; //skip one in 300
     while(!fTimes.eof())
     {
         string s;
