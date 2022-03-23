ae970a8c05cd0566d2a22d59799f6ea58efcd884
Aditya <adityadhakal@github.com>
2022-03-23 22:31:53 +0100

    1200-1500 300 images.

--- Examples/Stereo/stereo_kitti.cc
+++ Examples/Stereo/stereo_kitti.cc
@@ -89,7 +89,7 @@ int main(int argc, char **argv)
 
         //stop after 100 frames.
         count_images++;
-        if(count_images==1500)
+        if(count_images==300)
         {
             std::cout<<" --- More than 100 images --- we pause now and run postload "<<std::endl<<std::endl;
             std::cout<<"------------------------------------------------------------"<<std::endl;
@@ -320,7 +320,7 @@ void LoadImages(const string &strPathToSequence, vector<string> &vstrImageLeft,
     int new_counter = 0;
     //for(int i=0; i<nTimes_loop; i++)
     
-    for(int i=0; i<1500; i++)
+    for(int i=1200; i<1500; i++)
     {
         if(i%skip !=0){
             stringstream ss;
