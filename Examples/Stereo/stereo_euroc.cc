45728841491fb034c4244c6dabdf4186a1ec5c8e
Aditya <adityadhakal@github.com>
2022-03-24 16:22:02 +0100

    just merge at 1000

--- Examples/Stereo/stereo_euroc.cc
+++ Examples/Stereo/stereo_euroc.cc
@@ -216,7 +216,7 @@ int main(int argc, char **argv)
 
 
             std::cout<<ni<<" images completed\n";
-            if(ni==2272){
+            if(ni==1000){
                 std::cout<<" --- More than 200 images --- we pause now and run postload "<<std::endl<<std::endl;
             std::cout<<"------------------------------------------------------------"<<std::endl;
             std::cout<<"------------------------------------------------------------"<<std::endl;
